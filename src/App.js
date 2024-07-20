3b94a34aae755aa71c06bff6f6b1024db5182601
pr-askme <hi.priya.workspace@gmail.com>
2024-07-20 16:04:14 +0530

    Add: Finished challenge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const skills = [
+    { name: "HTML/CSS", color: "red", level: "👌" },
+    { name: "Javascript", color: "blue", level: "🤓" },
+    { name: "React", color: "green", level: "👶" },
+    { name: "Git & GitHub", color: "yellow", level: "👶" },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList skills={skills} />
+      </div>
     </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="priya_dp.jpg" alt="my-photo" />;
+}
+
+function Intro() {
+  return (
+    <>
+      <h1>Priya S</h1>
+      <p>
+        Front-end Developer; love to build communities. I love to play
+        Basketball & do Yoga in my free time. My favourite female-gazey shows
+        are "Desperate Housewives", "The Good wife"!
+      </p>
+    </>
+  );
+}
+
+function SkillList({ skills }) {
+  return (
+    <ul className="skill-list">
+      {skills.map((skill) => (
+        <Skill name={skill.name} color={skill.color} level={skill.level} />
+      ))}
+    </ul>
+  );
+}
+
+function Skill({ name, color, level }) {
+  return (
+    <li className="skill" style={{ backgroundColor: color }}>
+      {name} {level}
+    </li>
   );
 }
 
